9282a21943bf6bbf9f6cdbdb687ba0745bf29d7f
ttoysttory <betrue3d@gmail.com>
2020-09-03 16:37:45 +0900

    modified login/sign-up

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,13 +4,22 @@ from django import forms
 
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    # email = forms.EmailField(required=True)
     first_name = forms.Textarea()
     last_name = forms.Textarea()
 
     class Meta:
         model = User
         fields = ("username", "last_name", "first_name", "email", "password1", "password2")
+
+        # widgets = {
+        #     'username': forms.TextInput(attrs={'class': 'form-control', 'id': 'text_input'}),
+        #     'last_name': forms.TextInput(attrs={'class': 'form-control', 'id': 'text_input'}),
+        #     'first_name': forms.TextInput(attrs={'class': 'form-control', 'id': 'text_input'}),
+        #     'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        #     'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+        #     'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        # }
 
     def save(self, commit=True):
         user = super(CreateUserForm, self).save(commit=False)
